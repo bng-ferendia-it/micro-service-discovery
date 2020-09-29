58948fb49b41e887bc725af784a8299c849bf6d0
Daniel Bradea <daniel.bradea@ferendia.com>
2020-09-29 18:17:24 +0300

    Service-Discovery-Eureka

---
+++ src/test/java/com/ferendia/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferendia.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
